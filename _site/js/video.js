0d322e3175a06af4f5cd0aa7dd4f56b2728be37e
amuelb <amybrown@Amys-MacBook-Pro-2.local>
2014-01-29 20:54:31 -0500

    added pop up video

---
+++ _site/js/video.js
@@ -0,0 +1,30 @@
+var $dialog = $('#video');
+var $placeholder = $('#video-placeholder');
+
+var dialog = document.querySelector('dialog');
+    dialogPolyfill.registerDialog(dialog);
+
+$('#btn-open').on('click', function() {
+	$placeholder.html('<iframe src="http://player.vimeo.com/video/81295966?color=333333&amp;autoplay=1" width="700" height="394" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+	//.get() returns the native javascript for us to access
+	//when using jquery we dont have access to the original HTML elements directly, we are using jquery to manipulate them.
+	//EXAMPLE: 
+	//$dialog.show() - will call jquery's "show()" function
+	//$dialog.get(0).show() - will call javascripts native "show()" function
+	$dialog.get(0).showModal();
+	//.show() --> allows multiple dialogs
+	//.showModal() --> single dialog, with no user interaction below
+});
+
+
+$('#btn-close').on('click', function() {
+	//.get() returns the native javascript for us to access
+	//when using jquery we dont have access to the original HTML elements directly, we are using jquery to manipulate them.
+	//EXAMPLE: 
+	//$dialog.show() - will call jquery's "show()" function
+	//$dialog.get(0).show() - will call javascripts native "show()" function
+	$dialog.get(0).close();
+	$placeholder.html('');
+	//.show() --> allows multiple dialogs
+	//.showModal() --> single dialog, with no user interaction below
+});
